2ab18d19d98477247bb208bd93eaefff9d90634e
Egor Shramko <egorii2001@gmail.com>
2026-07-21 18:53:56 +0700

    Инициализация репозитория

---
+++ src/main/java/io/github/egorshramko/cbs/CbsBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.egorshramko.cbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/egorshramko/cbs/CbsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.egorshramko.cbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
