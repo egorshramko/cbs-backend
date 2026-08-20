68a354abe814a7e1ff8cf63ccfbb929b0ee288cf
Egor Shramko <egorii2001@gmail.com>
2026-08-20 14:43:35 +0700

    Убрал аннотацию Repository у MovieRepository

--- src/main/java/io/github/egorshramko/cbs/repository/MovieRepository.java
+++ src/main/java/io/github/egorshramko/cbs/repository/MovieRepository.java
@@ -1,11 +1,9 @@
 package io.github.egorshramko.cbs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import io.github.egorshramko.cbs.model.Movie;
 
-@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     
 }
