abe44fba4c68948b31054395af396af83d59e1b3
Egor Shramko <egorii2001@gmail.com>
2026-09-17 23:41:42 +0700

    Убрал неиспользуемые импорты в файлах и обновил gitignore

--- src/main/java/io/github/egorshramko/cbs/service/impl/SessionServiceImpl.java
+++ src/main/java/io/github/egorshramko/cbs/service/impl/SessionServiceImpl.java
@@ -1,17 +1,12 @@
 package io.github.egorshramko.cbs.service.impl;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import io.github.egorshramko.cbs.dto.sessions.MovieSessionResponse;
-import io.github.egorshramko.cbs.model.Cinema;
 import io.github.egorshramko.cbs.model.Session;
-import io.github.egorshramko.cbs.repository.CinemaRepository;
 import io.github.egorshramko.cbs.repository.SessionRepository;
 import io.github.egorshramko.cbs.service.SessionService;
 import io.github.egorshramko.cbs.util.mapper.SessionMapper;
